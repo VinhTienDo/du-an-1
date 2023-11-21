402ba8e3686d35c08317cf397748a8ce821fb2b8
vinhldph35167 <vinhldph35167@fpt.edu.vn>
2023-11-21 19:09:04 +0700

    .

--- app/src/main/java/fpoly/vinhldph35167/du_an_1/MainActivity.java
+++ app/src/main/java/fpoly/vinhldph35167/du_an_1/MainActivity.java
@@ -14,14 +14,19 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import fpoly.vinhldph35167.du_an_1.Dao.NhanVienDao;
 import fpoly.vinhldph35167.du_an_1.Fragment.fragment_change_pass;
@@ -35,6 +40,11 @@ import fpoly.vinhldph35167.du_an_1.Fragment.fragment_top;
 import fpoly.vinhldph35167.du_an_1.Model.NhanVien;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
+    private List<Integer> imageList;
+    private int currentPosition;
+    private Handler handler;
+    private Runnable runnable;
 
     DrawerLayout drawerLayout;
 
@@ -42,6 +52,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         FrameLayout frameLayout = findViewById(R.id.flContent);
@@ -52,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeAsUpIndicator(R.drawable.baseline_menu_24);
+
 
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -89,11 +102,47 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+        slideShow();
     }
     public boolean onOptionsItemSelected(@NonNull MenuItem item){
         if (item.getItemId() == android.R.id.home){
             drawerLayout.openDrawer(GravityCompat.START);
         }
         return super.onOptionsItemSelected(item);
+    }
+    public void slideShow(){
+        // Thay đổi danh sách hình ảnh thành các tài nguyên hợp lệ của bạn
+        imageList = new ArrayList<>();
+        imageList.add(R.drawable.banner1);
+        imageList.add(R.drawable.banner2);
+        imageList.add(R.drawable.banner1);
+
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                // Chuyển đổi hình ảnh hiện tại
+                currentPosition++;
+                if (currentPosition >= imageList.size()) {
+                    currentPosition = 0;
+                }
+                imageView.setImageResource(imageList.get(currentPosition));
+
+                // Lập lịch chạy lại Runnable sau 3 giây
+                handler.postDelayed(this, 2000);
+            }
+        };
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        currentPosition = -1;
+        handler.postDelayed(runnable, 0);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        handler.removeCallbacks(runnable);
     }
 }
