fe88efd4d769b42656fc9ef43c505dc8debf9475
vinhldph35167 <vinhldph35167@fpt.edu.vn>
2023-11-27 20:58:42 +0700

    .

---
+++ app/src/main/java/fpoly/vinhldph35167/du_an_1/ManHinhKhachHangDemo.java
@@ -0,0 +1,14 @@
+package fpoly.vinhldph35167.du_an_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManHinhKhachHangDemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_man_hinh_khach_hang_demo);
+    }
+}
